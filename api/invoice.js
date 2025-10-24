57e0911a456c1a94604f534fcc9c1d48ca5c880e
asusmdes-stack <asus.md.es@gmail.com>
2025-10-24 22:03:50 +0300

    Update invoice.js

--- api/invoice.js
+++ api/invoice.js
@@ -1,17 +1,16 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
-  const { user_id } = req.body;
+  const { user_id, init_data } = req.body;
   if (!user_id) return res.status(400).json({ error: "Missing user_id" });
 
   try {
-    // URL твоего бота на Render
     const botUrl = "https://telega-6jtc.onrender.com/create_invoice";
 
     const response = await fetch(botUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ user_id }),
+      body: JSON.stringify({ user_id, init_data }),
     });
 
     const result = await response.json();
